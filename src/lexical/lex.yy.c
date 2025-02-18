c102a98f48159de23dd72bb5a369508e59dc9237
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2025-02-18 15:16:46 +0100

    re structure

--- src/lexical/lex.yy.c
+++ src/lexical/lex.yy.c
@@ -536,7 +536,11 @@ int col=1;
  * down here because we want the user's section 1 to have been scanned first.
  * The user has a chance to override it with an option.
  */
+#ifdef _WIN32
+#include <io.h>
+#else
 #include <unistd.h>
+#endif
 #endif
 
 #ifndef YY_EXTRA_TYPE
