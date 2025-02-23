23ebe5974cc559f342de3fe8513571055206e858
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2025-02-23 08:50:52 +0100

    bat

--- scripts/command.bat
+++ scripts/command.bat
@@ -1,15 +1,9 @@
 @echo off
-REM Run Flex and Bison
 flex Lexical.l
 bison -d syntax.y
-
-REM Compile with GCC (Windows does NOT need -lfl or -ly)
 gcc lex.yy.c syntax.tab.c -o compilateur.exe -lm
-
-REM Run tests
 echo Test 1:
 compilateur.exe < test1.txt
-
 echo.
 echo Test 2:
 compilateur.exe < test2.txt
